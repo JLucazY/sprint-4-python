f491b77d5bf1fd1a24600969e6d727e5a6eb66dc
JLucazY <rm98458@fiap.com.br>
2023-11-12 15:37:54 -0300

    Projeto finalizado

--- login.py
+++ login.py
@@ -1,33 +1,52 @@
-import coletando_json
 import re
 import main
 
+#Dados usados para login dos usuários
+dados_usuarios = {
+    "Joao123": {
+        "nome": "João Lucas",
+        "senha": "0000"
+    },
+    "Breno123": {
+        "nome": "Breno Silva",
+        "senha": "0000"
+    }
+}
+
+#Funções usadas no código
 def validar_login(login):
     return re.match(r'^[a-zA-Z0-9]{6,12}$', login)
+
 def validar_nome(nome):
     return re.match(r"^[A-Za-zÀ-ÿ\s]+$", nome)
+
 def validar_senha(senha):
     return re.match(r'\d{1,5}', senha)
 
+#Função de entrar com usuário e senha
 def entrar_com_usuario():
+    while True:
+        login = input("Digite o login: ")
+        #verifica se os dados estão corretos
+        if login not in dados_usuarios.keys() or not validar_login(login):
+            print("Usuário inválido!")
+        else:
+            break
 
-        while True:
-            login = input("Digite o login: ")
-            if login not in coletando_json.u.keys() and validar_login(login):
-                print("Usuário inválido!")
-            else:
-                break
+    while True:
+        senha = input("Digite sua senha: ")
+        # verifica se os dados estão corretos
+        if senha != dados_usuarios[login]["senha"] or not validar_senha(senha):
+            print("Senha inválida!")
+        else:
+            break
 
-        while True:
-            senha = input("Digite sua senha: ")
-            if senha not in coletando_json.u[login].values() and validar_senha(senha):
-                print("Senha inválida!")
-            else:
-                break
-        print("Log in realizado com sucesso!")
-        main.main(login, coletando_json.u[login]["nome"], senha)
+    print("Log in realizado com sucesso!")
+    #Chama a função main do menu
+    main.main(login, senha, dados_usuarios[login]["nome"])
 
-def criar_usuario(u):
+#Função que cria o usuário, senha e nome
+def criar_usuario():
     while True:
         login = input("Digite o nome de login: ")
         if validar_login(login):
@@ -49,12 +68,16 @@ def criar_usuario(u):
         else:
             print("Senha deve conter de 1 a 5 caracteres e somente números!")
 
-    coletando_json.adicionar_login(login, nome, senha, u)
-    entrar(u)
+    novo_usuario = {
+        "nome": nome,
+        "senha": senha
+    }
+    dados_usuarios[login] = novo_usuario
+    entrar()
 
-
-
-def entrar(u):
+#Função que simula um menu de entrar/criar usuário
+def entrar():
+    print(dados_usuarios)
     while True:
         print("1 - Entrar\n2 - Criar usuário")
         escolha_usuario = input()
@@ -62,11 +85,12 @@ def entrar(u):
         match escolha_usuario:
             case '1':
                 entrar_com_usuario()
-                break  # Sai do loop se a escolha for válida
+                break
             case '2':
-                criar_usuario(u)
-                break  # Sai do loop se a escolha for válida
+                criar_usuario()
+                break
             case _:
                 print("Digite um número válido!")
 
-entrar(coletando_json.u)
+# Chama a função entrar para iniciar o programa
+entrar()
--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import json
 import os
+import re
 
+#Funções usadas no código
+def validar_letras(s):
+    return bool(re.match(r"^[A-Za-z]+$", s))
+def validar_numeros(s):
+    return s.isdigit()
 
+#Função que cria o sensor
 def coletar_e_criar_arquivo_sensor(login, nome, senha):
-    nome = input("Digite o nome do sensor: ")
-    temperatura = input("Digite a temperatura do sensor: ")
-    energia = input("Digite a energia consumida: ")
+    while True:
+        nome_sensor = input("Digite o nome do sensor: ")
+        if validar_letras(nome_sensor):
+            break
+        else:
+            print("Nome inválido!")
+    while True:
+        temperatura = input("Digite a temperatura do sensor: ")
+        if validar_numeros(temperatura):
+            break
+        else:
+            print("Digite apenas números!")
+    while True:
+        energia = input("Digite a energia consumida: ")
+        if validar_numeros(energia):
+            break
+        else:
+            print("Digite apenas números!")
 
     sensor_data = {
-        "nome": nome,
+        "nome": nome_sensor,
         "temperatura": temperatura,
         "energia": energia
     }
 
-    sensor_filename = f"{nome}_sensor.json"
+    sensor_filename = f"{nome_sensor}_sensor.json"
 
     with open(sensor_filename, 'w') as f:
         json.dump(sensor_data, f, indent=4)
@@ -21,6 +43,7 @@ def coletar_e_criar_arquivo_sensor(login, nome, senha):
     print(f"Dados do sensor foram registrados com sucesso!")
     main(login, nome, senha)
 
+#Função que exibe os sensores criados
 def exibir_dados_sensores(login, nome, senha):
     for filename in os.listdir('.'):
         if filename.endswith('_sensor.json'):
@@ -32,26 +55,48 @@ def exibir_dados_sensores(login, nome, senha):
                 print()
     main(login, nome, senha)
 
+#Função para exibir os sensores quando o código é encerrado
+def exibir_sensores_final():
+    for filename in os.listdir('.'):
+        if filename.endswith('_sensor.json'):
+            with open(filename, 'r') as f:
+                sensor_data = json.load(f)
+                print(f"Sensor: {sensor_data['nome']}")
+                print(f"Temperatura: {sensor_data['temperatura']} °C")
+                print(f"Energia Consumida: {sensor_data['energia']} kw")
+
+#função que simula o menu
 def main(login, nome, senha):
-    print("""
-        Menu
-    1 - Cadastrar dados dos sensores
-    2 - Exibir dados dos sensores
-    3 - Encerrar
-    """)
+    nome_usuario = nome
+    while True:
+        print("""
+            Menu
+        1 - Cadastrar dados dos sensores
+        2 - Exibir dados dos sensores
+        3 - Encerrar
+        """)
 
-    escolha_usuario = input()
+        escolha_usuario = input()
 
-    match escolha_usuario:
-        case '1':
-            coletar_e_criar_arquivo_sensor(login, nome, senha)
-        case '2':
-            exibir_dados_sensores(login, nome, senha)
-        case '3':
-            print(f"""
-Dados coletados:
-nome - {nome}
-login - {login}
-senha - {senha}
-""")
+        match escolha_usuario:
+            case '1':
+                coletar_e_criar_arquivo_sensor(login, nome, senha)
+                break
+            case '2':
+                exibir_dados_sensores(login, nome, senha)
+                break
+            case '3':
+            #Mostra um resumo da operação
+                print(f"""
+            Dados coletados:
+            Nome do usuário - {senha}
+            Login - {login}
+            Senha - {nome_usuario}
+            Informações dos Sensores:
+            """)
+                exibir_sensores_final()
+                print("\nObrigado por usar o programa!\nEncerrando...")
+                exit()
+            case _:
+                print("Digite um número válido!")
 
